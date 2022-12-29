2e430b63de548f7d0a3cf3146ec2f3ab6402aecb
Andrew MacIsaac <amacisaac95@gmail.com>
2022-12-28 21:33:35 -0700

    commit tree c105fc85e8f5bd10fedabea5e2dce7511aac42a3

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+const names = ["Tom", "Billy", "James"];
+const event = ["surprise"];
+
+function writeCards(names, event) {
+    let output = [];
+  for (let i = 0; i < names.length; i++) {
+     output[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    
+  }
+  return output;
+  
+}
+
+writeCards(names, "surprise");
+
+function countDown()
+{
+    let counter = 10;
+    while (counter >=0)
+    {
+        console.log(counter);
+        counter--;
+    }
+}
