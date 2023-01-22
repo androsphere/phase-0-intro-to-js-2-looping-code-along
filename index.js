d5176a014d07c18a83d0771153e6328ff2f99d1e
Andrew MacIsaac <amacisaac95@gmail.com>
2023-01-21 20:43:56 -0700

    Lab complete

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const names = ["Tom", "Billy", "James"];
+const event = ["surprise"];
+
+function writeCards(names, event) {
+    let output = [];
+  for (let i = 0; i < names.length; i++) {
+     output[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`; 
+  }
+  return output;
+}
+function countDown(int){
+    let i = int;
+    while(i>=0){
+        console.log(i);
+        i--;
+    }
+}
